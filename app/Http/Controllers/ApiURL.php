2fd508dc3c785659155bdadb3da029d5ae43d992
Панкратова Анастасия < @ana>
2025-02-05 17:10:36 +0700

    api

--- app/Http/Controllers/ApiURL.php
+++ app/Http/Controllers/ApiURL.php
@@ -11,7 +11,7 @@ class ApiURL extends Controller
     {
 
         $key=$request->input('key');
-        dd($key);
+
         if ($key!='4a8f22d183adbdd7b4adcaa7d7a935fe')
             abort(401);
         return URL::temporarySignedRoute('person.index',now()->addDays(7),['id'=>$id]);
