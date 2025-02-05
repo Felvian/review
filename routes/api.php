3c3bc83a37a6cc6da02d29cfa5c92f1ab3cf2cbc
Панкратова Анастасия < @ana>
2025-02-05 16:47:53 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/url',[\App\Http\Controllers\ApiURL::class,'index']);
+Route::get('/{id}/url',[\App\Http\Controllers\ApiURL::class,'index']);
