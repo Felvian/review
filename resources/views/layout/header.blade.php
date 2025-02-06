5ee0f6b51a62d1b3ba8ac6599b148473cc399fc4
Панкратова Анастасия < @ana>
2025-02-06 12:11:58 +0700

    blade

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -6,8 +6,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel=”icon”  sizes='16x16' href=”{{asset('images/favicon32.png')}}” type=”image/png”>.
-    <link rel=”icon”  sizes='32x32' href=”{{asset('images/favicon32.png')}}” type=”image/png”>.
+    <link rel=”icon”  sizes='16x16' href=”{{asset('images/favicon-16x16.png')}}” type=”image/png”>
+    <link rel=”icon”  sizes='32x32' href=”{{asset('images/favicon-32x32.png')}}” type=”image/png”>
+    <link rel="manifest" href="{{asset('images/site.webmanifest')}}">
     @vite('resources/css/app.css')
     <title>Отзывы Алатрацев.Недвижимость</title>
 </head>
