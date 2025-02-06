92189cb98f3a13d74ab60e4182218b88006d20fd
Панкратова Анастасия < @ana>
2025-02-06 11:54:31 +0700

    blade

--- resources/views/person/thanks.blade.php
+++ resources/views/person/thanks.blade.php
@@ -1,13 +1,4 @@
-<!doctype html>
-<html lang="RU">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Отзывы Алатрацев.Недвижимость</title>
-</head>
+@include('layout.header')
 <body class="m-3 flex justify-center items-center flex-col">
 <div class=" mt-4 mb-6 flex justify-center items-center" >
     <div>
